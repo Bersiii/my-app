e2997b9f1854eb556b150247f5c933d20601a54a
Betelhem sisay <bety87696@gmail.com>
2026-07-29 10:08:34 +0300

    feat: add initial configuration and assets for the project
    - Created babel.config.js for Babel configuration with presets for Expo and NativeWind.
    - Added constants for icons and images in constants/icons.ts and constants/images.ts respectively.
    - Defined interfaces for Movie, TrendingMovie, MovieDetails, and TrendingCardProps in interfaces/interfaces.d.ts.
    - Configured Metro bundler in metro.config.js to work with NativeWind.
    - Added TypeScript definitions for NativeWind in nativewind-env.d.ts.
    - Set up Tailwind CSS configuration in tailwind.config.js with custom theme colors.
    - Declared module for PNG images in types/images.d.ts.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,105 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import React from "react";
+import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import { ImageBackground, Image, Text } from "react-native";
+import "../global.css";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+// type TabIconProps = {
+//   icon: any;
+//   name: string;
+//   focused: boolean;
+// };
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const TabIcon = ({
+  icon,
+  name,
+  focused,
+}: {
+  icon: any;
+  name: string;
+  focused: boolean;
+}) => {
+  return (
+    <>
+      {focused ? (
+        <ImageBackground
+          source={images.highlight}
+          className="flex flex-row w-full flex-1 min-w-[100px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden gap-3"
+        >
+          <Image source={icon} tintColor="#151213" className="size-5" />
+          <Text className="text-secondary text-base font-semibold">{name}</Text>
+        </ImageBackground>
+      ) : (
+        <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+      )}
+    </>
+  );
+};
 
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D13",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 5,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D13",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.home} name="Home" focused={focused} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.search} name={"search"} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.save} name={"Save"} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.person} name={"Profile"} focused={focused} />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,9 @@
 import { Text, View } from "react-native";
 
-
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-green-900 text-5xl font-bold">Welcome!</Text>
+      <Text className="text-accent text-5xl font-bold">profie</Text>
     </View>
   );
 }
