6582f1f4119ac1146ea32b4f23269ade02489f48
Bersiii <madonna1621@gmail.com>
2026-08-05 00:51:57 +0300

    Add ChatRoom and ChatHeader components; implement message input and display functionality

--- app/(app)/ChatRoom.js
+++ app/(app)/ChatRoom.js
@@ -1,16 +1,36 @@
-import { View } from "react-native";
-import React from "react";
-import { useLocalSearchParams , useRouter} from "expo-router";
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import ChatHeader from "../../component/chatHeader";
+import MessageList from "../../component/MessageList";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
 export default function ChatRoom() {
-  const item = useLocalSearchParams();  
-  const router =useRouter();
+  const item = useLocalSearchParams();
+  const router = useRouter();
+  const [messages, setMessages] = useState([]);
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <StatusBar style="dark"/>
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
       <ChatHeader user={item} router={router} />
+      <View className="border-b border-gray-300" />
+      <View className="flex-1 bg-neutral-100 justify-between overflow-visible">
+        <View className="flex1">
+          <MessageList messages={messages} />
+        </View>
+      </View>
+      <View className="pt2" style={{ marginBottom: hp(5) }}>
+        <View className="flex-row justify-between items-center mx-3">
+          <View className="flex-row justify-between bg-white border p-2 border-neutral-300 rounded-full p">
+            <TextInput placeholder="Type message..." 
+            className="flex-1 mr-2"/>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
--- component/chatHeader.js
+++ component/chatHeader.js
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { Entypo, Ionicons, Octicons } from "@expo/vector-icons";
 
-const  ChatHeader= () => {
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
+import { Image } from "expo-image";
+
+const ChatHeader = ({ user, router }) => {
   return (
-    <View>
-      <Text>ChatHeader</Text>
-    </View>
-  )
-}
+    <Stack.Screen
+      options={{
+        title: "",
+        headerShadowVisible: false ,
+        headerLeft: () => (
+          <View className="flex-row items-center gap-1 mt-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <Entypo name="chevron-left" size={hp(4)} color="black" />
+            </TouchableOpacity>
+            <View className="bg-neutral-100 flex-row items-center  px-2 py-1 rounded-full gap-5 mb-2">
+              <Image
+                source={user?.profileUrl}
+                style={{ height: hp(5), width: hp(5), borderRadius: hp(2.5) }}
+              />
+              <Text className="text-lg font-medium text-neutral-700">
+                {user?.username || "User"}{" "}
+              </Text>
+            </View>
+          </View>
+        ),
+        headerRight: () => (
+          <View className="flex-row items-center gap-5 mt-5">
+            <TouchableOpacity>
+              <Ionicons name="call" size={hp(3)} color="#737373" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Ionicons name="videocam" size={hp(3)} color="#737373" />
+            </TouchableOpacity>
+          </View>
+        ),
+      }}
+    />
+  );
+};
 
-export default ChatHeader
+export default ChatHeader;
