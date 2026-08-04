099347a8f7947c74e7ef8c86abf5e616f9d665a9
Bersiii <madonna1621@gmail.com>
2026-08-04 11:51:34 +0300

    Enhance ChatItem component with noBorder prop and update ChatList to pass router

--- component/ChatList.js
+++ component/ChatList.js
@@ -1,8 +1,10 @@
 import { View, FlatList } from "react-native";
 import React from "react";
 import ChatItem from "./chatitem";
+import { useRouter } from "expo-router";
 
 const ChatList = ({ users }) => {
+  const router = useRouter();
   return (
     <View className="flex-1">
       <FlatList
@@ -10,7 +12,14 @@ const ChatList = ({ users }) => {
         contentContainerStyle={{ flex: 1, paddingVertical: 25 }}
         keyExtractor={(item) => item.id || item.userId}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item, index }) => <ChatItem user={item} Index={index} />}
+        renderItem={({ item, index }) => (
+          <ChatItem
+            user={item}
+            Index={index}
+            noBorder={index + 1 === users.length}
+            router={router}
+          />
+        )}
       />
     </View>
   );
--- component/chatitem.js
+++ component/chatitem.js
@@ -1,14 +1,19 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
-const ChatItem = ({ user }) => {
+const ChatItem = ({ user, noBorder }) => {
   return (
-    <TouchableOpacity className="flex-row items-center px-4 py-3 border-b border-neutral-200">
-      <View className="w-12 h-12 rounded-full bg-gray-200 justify-center items-center">
-        {user.profileUrl ? (
+    <TouchableOpacity
+      className={`flex-row items-center px-4 py-3 ${
+        noBorder ? "" : "border-b border-neutral-200"
+      }`}
+    >
+      {/* 1. Profile Avatar */}
+      <View className="w-12 h-12 rounded-full overflow-hidden justify-center items-center bg-neutral-200">
+        {user?.profileUrl ? (
           <Image
             source={{ uri: user.profileUrl }}
-            className="w-12 h-12 rounded-full bg-gray-200"
+            className="w-12 h-12 rounded-full"
           />
         ) : (
           <Image
@@ -18,15 +23,24 @@ const ChatItem = ({ user }) => {
         )}
       </View>
 
-      <View className="ml-4 flex">
-        <View className="flex-row justify-between ">
-          <Text className="text-base font-bold text-white">
-            {user.username ? user.username[0]?.toUpperCase() : "U"}
+      {/* 2. User Info & Message Details */}
+      <View className="ml-4 flex-1 justify-center gap-1">
+        <View className="flex-row justify-between items-center">
+          {/* Full Username */}
+          <Text className="text-base font-semibold text-neutral-800">
+            {user?.username || "User"}
           </Text>
-          <Text>Time</Text>
+          {/* Timestamp */}
+          <Text className="text-xs text-neutral-500">12:30 PM</Text>
         </View>
 
-        <Text className="text-sm text-black pt-5">Last Text</Text>
+        {/* Last Message */}
+        <Text
+          className="text-sm text-neutral-500 font-medium"
+          numberOfLines={1}
+        >
+          Last text message...
+        </Text>
       </View>
     </TouchableOpacity>
   );
